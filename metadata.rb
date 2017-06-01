306524e2c2742da852630465912efaf62121c8c8
Bakh Inamov <b@chef.io>
2017-06-01 11:11:53 -0700

    pins webpi to 3.0.0
    thanks @ericheiser

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'b@chef.io'
 license 'all_rights'
 description 'Installs/Configures IIS as a web load-balancer'
 long_description 'Creates an IIS Server Farm and adds Servers to the farm.'
-version '0.2.4'
+version '0.2.5'
 
 supports 'windows'
 
-depends 'webpi'
+depends 'webpi', '= 3.0.0'
 depends 'iis'
 depends 'compat_resource'
